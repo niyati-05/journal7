f4a35b1c28b16d860a2d505641b6b0f205fce703
niyati05 <niyatigogri05@gmail.com>
2025-11-17 14:29:54 +0530

    updated

--- scores_array.py
+++ scores_array.py
@@ -9,7 +9,10 @@ else:
 
 total = sum(scores)
 average = total / len(scores)
-
+maximum = max(scores)
+minimum = min(scores)
 
 print("Sum of scores:", total)
 print("Average of scores:", average)
+print("Maximum score:", maximum)
+print("Minimum score:", minimum)
